df75b4529e9e9f7f000d06e45ec287681ca88561
CreativeToby02 <akannitobi2@gmail.com>
2022-12-11 09:15:38 -0800

    fiixed

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,8 +3,8 @@
 /**
  * main - Entry point
  *
- * Return - Always 0 (Success)
- */
+ * Return - always 0
+ * /
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
